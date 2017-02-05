0b88c89ff00107efe1ea28fe2dad4c6468f92548
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-05 17:44:33 +0900

    Update zlibpkg to 1.2.11 Add zlibpkg 1.2.11 binaries Add check method

--- packages/zlibpkg.rb
+++ packages/zlibpkg.rb
@@ -1,15 +1,30 @@
 require 'package'
 
 class Zlibpkg < Package
-  version '1.2.8'
+  version '1.2.11'
+  source_url 'http://www.zlib.net/zlib-1.2.11.tar.gz'
+  source_sha1 'e6d119755acdf9104d7ba236b1242696940ed6dd'
   binary_url ({
-    armv7l: 'https://dl.dropboxusercontent.com/s/7y34bh0wph4iipa/zlibpkg-1.2.8-chromeos-armv7l.tar.xz',
-    i686: 'https://dl.dropboxusercontent.com/s/ljhhvr12u1izayj/zlib-1.2.8-chromeos-i686.tar.gz?token_hash=AAEABTatYkxOOybZGoCj3Kg_DKEbFbSfolzZklfHwCsP_A&dl=1',
-    x86_64: 'https://dl.dropboxusercontent.com/s/h4lqj0rnand0jqu/zlib-1.2.8-chromeos-x86_64.tar.gz?token_hash=AAGabAMhX4CGpzhpkcuKMmmWPxFZDiNOC-r9B0o7x4D7eQ&dl=1'
+    armv7l: 'https://github.com/jam7/chromebrew/releases/download/binaries/zlibpkg-1.2.11-chromeos-armv7l.tar.xz',
+    i686:   'https://github.com/jam7/chromebrew/releases/download/binaries/zlibpkg-1.2.11-chromeos-i686.tar.xz',
+    x86_64: 'https://github.com/jam7/chromebrew/releases/download/binaries/zlibpkg-1.2.11-chromeos-x86_64.tar.xz',
   })
   binary_sha1 ({
-    armv7l: '85980c0e6765e6264b191db1fa4c58508e663d3f',
-    i686: 'e02974780bfb3bf46940183043d15897a765ab4e',
-    x86_64: 'cb764e22b68b7e2884372708b5b585d11efca972'
+    armv7l: '195db0418f70b593e7a3d6530309fbd1b2f3200d',
+    i686:   'a7a12709e1b016bbd2e3c8aa0969addf129bc4c6',
+    x86_64: 'ae78cdddb2b74a4fa87b79fc04345d5213ab1a43',
   })
+
+  def self.build
+    system "./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make check"
+  end
 end
