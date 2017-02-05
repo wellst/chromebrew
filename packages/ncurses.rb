3f3c5699eb2093ee6e71eb91cbcb0960e123f449
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-05 18:47:12 +0900

    Change to not install static libraries

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Ncurses < Package
-  version '6.0-1'
+  version '6.0-2'
   source_url 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz'
   source_sha1 'acd606135a5124905da770803c05f1f20dd3b21c'
 
@@ -9,12 +9,12 @@ class Ncurses < Package
   depends_on "ncursesw"
 
   def self.build
-    system './configure ' \
-	    'CFLAGS=" -fPIC" ' \
-	    '--without-debug ' \
-	    '--prefix=/usr/local ' \
-	    '--with-shared ' \
-	    '--with-cxx-shared '
+    system './configure',
+	    '--prefix=/usr/local',
+	    '--without-normal',
+	    '--with-shared',
+	    '--with-cxx-shared',
+	    '--without-debug'
     system "make"
   end
 
--- packages/ncursesw.rb
+++ packages/ncursesw.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Ncursesw < Package
-  version '6.0'
+  version '6.0-1'
   source_url 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz'
   source_sha1 'acd606135a5124905da770803c05f1f20dd3b21c'
 
@@ -18,12 +18,12 @@ class Ncursesw < Package
       end
     end
     # Build ncursesw
-    system './configure ' \
-	    'CFLAGS=" -fPIC" ' \
-	    '--without-debug ' \
-	    '--prefix=/usr/local ' \
-	    '--with-shared ' \
-	    '--with-cxx-shared ' \
+    system './configure',
+	    '--prefix=/usr/local',
+	    '--without-normal',
+	    '--with-shared',
+	    '--with-cxx-shared',
+	    '--without-debug',
 	    '--enable-widec'
     system "make"
   end
