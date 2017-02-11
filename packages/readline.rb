8a18fe90f14487ee2fb2df03c8728053c0a03d0b
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-11 15:30:10 +0900

    Update patch to 7.0p3 Change configure options Correct dependencies Add readline 7.0p3 binaries

--- packages/readline.rb
+++ packages/readline.rb
@@ -1,14 +1,31 @@
 require 'package'
 
 class Readline < Package
-  version '6.3'
-  source_url 'http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz'
-  source_sha1 '017b92dc7fd4e636a2b5c9265a77ccc05798c9e1'
+  version '7.0p3'
+  source_url 'ftp://ftp.gnu.org/gnu/readline/readline-7.0.tar.gz'
+  source_sha1 'd9095fa14a812495052357e1d678b3f2ac635463'
+  binary_url ({
+    armv7l: 'https://github.com/jam7/chromebrew/releases/download/binaries/readline-7.0p3-chromeos-armv7l.tar.xz',
+    i686:   'https://github.com/jam7/chromebrew/releases/download/binaries/readline-7.0p3-chromeos-i686.tar.xz',
+    x86_64: 'https://github.com/jam7/chromebrew/releases/download/binaries/readline-7.0p3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha1 ({
+    armv7l: 'f5146136956a0e15e0ad4744652c0998dbeccbcd',
+    i686:   '3e0d522034d3b0615b335238765c9f3c59154b3c',
+    x86_64: 'dcc64b502c372c037ea78817a8f504171921ea6b',
+  })
 
   depends_on 'buildessential'
+  depends_on 'patch'
+  depends_on 'ncurses'
+  rdepends_on 'ncurses'
 
   def self.build
-    system "CC='gcc' ./configure"
+    system "wget -r -N -nd --no-parent ftp://ftp.gnu.org/gnu/readline/readline-7.0-patches -P readline-7.0-patches"
+    # system "for i in readline-7.0-patches/*.sig; do gpg $i; done"
+    system "for i in readline-7.0-patches/readline70-???; do patch < $i; done"
+
+    system "CC='gcc' ./configure --disable-static --with-curses"
     system "make"
   end
 
