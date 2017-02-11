91f49629fab028bce02db479981cf705363e86a5
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-11 21:34:58 +0900

    Update flex to 2.6.3 Add flex 2.6.3 binaries Add check method

--- packages/flex.rb
+++ packages/flex.rb
@@ -1,19 +1,34 @@
 require 'package'
 
 class Flex < Package
-  version '2.5.39-1'
-  source_url 'http://fossies.org/linux/misc/flex-2.6.0.tar.gz'
-  source_sha1 'cfe10b5de4893ced356adc437e78018e715818c3'
+  version '2.6.3'
+  source_url 'https://github.com/westes/flex/releases/download/v2.6.3/flex-2.6.3.tar.gz'
+  source_sha1 '8008eb0e4ccc634c334ac1d9e04716be2ba527a9'
+  binary_url ({
+    armv7l: 'https://github.com/jam7/chromebrew/releases/download/binaries/flex-2.6.3-chromeos-armv7l.tar.xz',
+    i686:   'https://github.com/jam7/chromebrew/releases/download/binaries/flex-2.6.3-chromeos-i686.tar.xz',
+    x86_64: 'https://github.com/jam7/chromebrew/releases/download/binaries/flex-2.6.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha1 ({
+    armv7l: '3669cb2eb9fe05120b61fc493fe032b670213be9',
+    i686:   'f6d1bbbe1c85533402620c4e41fc19c2cb24cf5e',
+    x86_64: '6654f4f8f85c3a87c023624ef1a788661f7ef52e',
+  })
 
   depends_on 'm4'
   depends_on 'bison'
+  rdepends_on 'm4'
 
   def self.build
-    system "./configure CFLAGS=\" -fPIC\""
+    system "./configure", "--with-pic"
     system "make"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make check"
   end
 end
