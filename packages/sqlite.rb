ce22ffcc9a6ec60746adf7243adc425bd5288990
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-12 17:29:42 +0900

    Update sqlite to 3.16.2 Disable static library

--- packages/sqlite.rb
+++ packages/sqlite.rb
@@ -1,16 +1,16 @@
-require 'package'                                                      	# include package class file
+require 'package'
  
-class Sqlite < Package                                            	# name the package and make it a Package class instance
-  version '3.10'                                               	                                      # software version
-  source_url 'http://www.sqlite.org/2016/sqlite-autoconf-3100000.tar.gz'     # software source tarball url
-  source_sha1 '7be6e6869d0d2d9fe3df71b5c65f065dd2325f58'          	# source tarball sha1 sum
+class Sqlite < Package
+  version '3.16.2'
+  source_url 'http://www.sqlite.org/2017/sqlite-autoconf-3160200.tar.gz'
+  source_sha1 '64ca578ad44a94115b1db0406740e14288f74bb8'
 
-  def self.build                                                  # self.build contains commands needed to build the software from source
-    system "./configure"
-    system "make"                                                 # ordered chronologically
+  def self.build
+    system "./configure", "--disable-static", "--enable-shared", "--with-pic"
+    system "make"
   end
   
-  def self.install                                                # self.install contains commands needed to install the software on the target system
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"          # remember to include DESTDIR set to CREW_DEST_DIR - needed to keep track of changes made to system
-  end                                                             # during installation
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
 end
