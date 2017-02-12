72f8d4a54835390755652c822fface21b82f517b
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-12 10:02:02 +0900

    Change to use ftp to use proxy Change to compile source using single core

--- packages/groff.rb
+++ packages/groff.rb
@@ -2,12 +2,12 @@ require 'package'
  
 class Groff < Package
   version '1.22.3'
-  source_url 'http://ftp.gnu.org/gnu/groff/groff-1.22.3.tar.gz'
+  source_url 'ftp://ftp.gnu.org/gnu/groff/groff-1.22.3.tar.gz'
   source_sha1 '61a6808ea1ef715df9fa8e9b424e1f6b9fa8c091'
   
   def self.build
     system './configure'
-    system 'make'
+    system 'make', '-j1'
   end
   
   def self.install
