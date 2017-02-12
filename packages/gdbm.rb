37e398ae81447835ba20248773340b34d59327f3
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-12 10:16:32 +0900

    Modify configure option to disable static library Add check method Increment version number since configuration option is changed

--- packages/gdbm.rb
+++ packages/gdbm.rb
@@ -1,16 +1,20 @@
 require 'package'
  
 class Gdbm < Package
-  version '1.12'
+  version '1.12-1'
   source_url 'ftp://ftp.gnu.org/gnu/gdbm/gdbm-1.12.tar.gz'
   source_sha1 '86513e8871bb376bc014e9e5a2d18a8e0a8ea2f5'
   
   def self.build
-    system './configure'
+    system './configure', '--disable-static', '--enable-shared', '--with-pic'
     system 'make'
   end
   
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make check"
   end
 end
