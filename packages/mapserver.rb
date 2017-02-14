798e10baedd9752334ebe144b985afb0cc5cff69
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-14 21:16:44 +0900

    trying to compile it. need strlcat and strlcpy.  Also strrstr

--- packages/mapserver.rb
+++ packages/mapserver.rb
@@ -15,14 +15,23 @@ class Mapserver < Package
   depends_on 'libxml2'
 
   def self.build
+    system "mkdir", "build"
+    Dir.chdir "build" do
+      system "cmake", ".."
+      # -DWITH_FRIBIDI=0 -DWITH_HARFBUZZ=0 -DWITH_CAIRO=0 -DWITH_FCGI=0 -DWITH_POSTGIS=0
+      # -DWITH_GIF=0
+      system "make"
+    end
     # Mapserver supports only tiff3, but we need tiff4 to use BigTIFF.
     # We use tiff4 through gdal as described at
     # http://lists.osgeo.org/pipermail/mapserver-users/2009-June/061792.html
-    system "./configure CFLAGS=\" -fPIC\" --with-gdal=/usr/local/bin/gdal-config --with-ogr --with-png --with-jpeg --without-pdf --with-agg --with-proj --with-threads --with-geos=/usr/local/bin/geos-config --with-wfs --with-wmsclient --with-wfsclient --with-curl-config=/usr/local/bin/curl-config --with-xml2-config=/usr/local/bin/xml2-config"
-    system "make"
+#    system "./configure CFLAGS=\" -fPIC\" --with-gdal=/usr/local/bin/gdal-config --with-ogr --with-png --with-jpeg --without-pdf --with-agg --with-proj --with-threads --with-geos=/usr/local/bin/geos-config --with-wfs --with-wmsclient --with-wfsclient --with-curl-config=/usr/local/bin/curl-config --with-xml2-config=/usr/local/bin/xml2-config"
+#    system "make"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    Dir.chdir "build" do
+      system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    end
   end
 end
