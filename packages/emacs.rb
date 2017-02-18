2691a6cbb88f1d8016d9f7a08a0ef775f78a196c
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-18 13:05:36 +0900

    Correct dependencies and add run-time dependency

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -5,10 +5,12 @@ class Emacs < Package
   source_url 'ftp://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.xz'
   source_sha1 '983e457971e3e3c8964d039c113033f98132b8a8'
 
+  depends_on "zlibpkg"
   depends_on "diffutils"
   depends_on "m4"
   depends_on "autoconf"
   depends_on "automake"
+  rdepends_on
 
   def self.build
     system "./configure --prefix=/usr/local --without-x --without-makeinfo --without-selinux"
