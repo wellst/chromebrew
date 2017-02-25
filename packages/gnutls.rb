d0cbeb0bd7cf4e5214785b123cd3998c3d715266
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-25 09:09:18 +0900

    Update GnuTLS to 3.5.9 Correct dependencies

--- packages/gnutls.rb
+++ packages/gnutls.rb
@@ -1,20 +1,41 @@
 require 'package'
 
 class Gnutls < Package
-  version '3.3.15'
-  source_url 'ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-3.3.15.tar.xz'
-  source_sha1 'd7f66b0aeaf48ff8621cc1913230635ef672f0a4'
+  version '3.5.9'
+  source_url 'ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/gnutls-3.5.9.tar.xz'
+  source_sha1 'f3f184a92f128af1c2fb29b29a4d325af65694a5'
 
   depends_on 'buildessential'
-  depends_on 'nettle'
   depends_on 'pkgconfig'
+  depends_on 'zlibpkg'
+  depends_on 'libunistring'
+  depends_on 'gmp'
+  depends_on 'nettle'
+  depends_on 'libtasn1'
+  depends_on 'libffi'
+  depends_on 'p11kit'
+  depends_on 'libunbound'
+
+  rdepends_on 'libunistring'
+  rdepends_on 'gmp'
+  rdepends_on 'nettle'
+  rdepends_on 'libtasn1'
+  rdepends_on 'libffi'
+  rdepends_on 'p11kit'
+  rdepends_on 'libunbound'
 
   def self.build
-    system "./configure"
+    system "./configure", "--enable-shared", "--disable-static", "--with-pic",
+      "--with-system-priority-file=#{CREW_PREFIX}/etc/gnutls/default-priorities",
+      "--with-unbound-root-key-file=#{CREW_PREFIX}/etc/unbound/root.key"
     system "make"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make check"
   end
 end
