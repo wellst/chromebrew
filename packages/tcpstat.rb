7f2dcad554fd21159c8a6a35a36131c9d2a1ca71
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-26 09:29:43 +0900

    Change source code URL

--- packages/tcpstat.rb
+++ packages/tcpstat.rb
@@ -2,7 +2,10 @@ require 'package'
 
 class Tcpstat < Package
   version '1.5' 
-  source_url 'http://www.frenchfries.net/paul/tcpstat/tcpstat-1.5.tar.gz' 
+
+  # original source is not available, so change url to one of the mirrors (identical sha1)
+  # source_url 'http://www.frenchfries.net/paul/tcpstat/tcpstat-1.5.tar.gz' 
+  source_url 'https://launchpad.net/ubuntu/+archive/primary/+files/tcpstat_1.5.orig.tar.gz' 
   source_sha1 '3881edafe2a45c807a6f197792251036c599ec50'
 
   def self.build
